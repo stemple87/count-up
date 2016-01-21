1070666e743ee836146d4acd36f41f9f71d45fd0
Ryan Brown and Simon Temple <browneryan87@gmail.com>
2016-01-21 11:12:47 -0800

    Add for loop functionality to scripts.js.

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+var getRemainder = function(input1, counter) {
+  var input1;
+  var counter;
+  var resultsArray = [];
+  for (var i = counter; i <= input1; i += counter) {
+    return i;
+  }
+}
+
+
+// var results = input1 / counter;
+// results = Math.floor(results);
+
+// resultsArray.push(i);
+
+//     var resultsArray = [];
+//     resultsArray.push(counter + i);
+//     return resultsArray;
+// }
